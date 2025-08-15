9f6148b9b896c53d3ade4408e87e28346e75b87a
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2025-08-15 16:05:07 +0530

    db connection implemented

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ const dotenv = require("dotenv").config();
 
 const app = express();
 
+//middleware
+
+app.use(express.json());
+
+//routes
+
+
+//Start the Server
+
+const PORT = process.env.PORT || 7001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
