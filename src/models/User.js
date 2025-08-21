08446ef7883d1f2c282515687b9d415b06da4829
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2025-08-21 14:11:14 +0530

    user model created

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        required:true,
+        enum:["user","admin","manager"]
+    }
+})
+
+
+module.exports = mongoose.model("User", userSchema);
